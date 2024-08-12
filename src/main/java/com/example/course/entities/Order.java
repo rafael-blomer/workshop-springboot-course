27cb93df0898b6fe3838e9ed0e54b18cadb3b3e1
Abc Bolinhas <abc@bolinhas.com>
2024-08-12 14:54:06 -0300

    criando OrderItem

--- src/main/java/com/example/course/entities/Order.java
+++ src/main/java/com/example/course/entities/Order.java
@@ -2,17 +2,21 @@ package com.example.course.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import com.example.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -30,6 +34,9 @@ public class Order implements Serializable {
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
+
+	@OneToMany(mappedBy = "id.order", fetch = FetchType.EAGER)
+	private Set<OrderItem> items = new HashSet<OrderItem>();
 
 	public Order() {
 	}
@@ -72,6 +79,10 @@ public class Order implements Serializable {
 	public void setOrderStatus(OrderStatus orderStatus) {
 		if (orderStatus != null)
 			this.orderStatus = orderStatus.getCode();
+	}
+
+	public Set<OrderItem> getItems() {
+		return items;
 	}
 
 	@Override
--- src/main/java/com/example/course/entities/Product.java
+++ src/main/java/com/example/course/entities/Product.java
@@ -6,6 +6,7 @@ import java.util.Objects;
 import java.util.Set;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -26,7 +27,7 @@ public class Product implements Serializable {
 	private Double price;
 	private String imgUrl;
 
-	@ManyToMany
+	@ManyToMany(fetch = FetchType.EAGER)
 	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<Category>();
 
