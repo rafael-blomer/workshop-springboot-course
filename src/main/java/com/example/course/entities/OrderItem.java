956e01605a0c716ec5c2aa9dbde07f3153c14dbd
Abc Bolinhas <abc@bolinhas.com>
2024-08-13 16:00:50 -0300

    metodos subtotal e total

--- src/main/java/com/example/course/entities/OrderItem.java
+++ src/main/java/com/example/course/entities/OrderItem.java
@@ -65,6 +65,10 @@ public class OrderItem implements Serializable{
 	public void setPrice(Double price) {
 		this.price = price;
 	}
+	
+	public Double getSubtotal() {
+		return getPrice() * getQuantity();
+	}
 
 	@Override
 	public int hashCode() {
